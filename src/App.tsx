c4b7d6eeaabdaecd781076b58067951a6294a477
Haneen Mahdin <haneenmahdin@gmail.com>
2022-02-19 17:08:05 +0530

    core: add function to read config data

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,19 @@
 import './App.css'
-import { AvatarShadowType, Header } from './components/Header';
+import { Header } from './components/Header';
 import { Links } from "./components/Links";
-import { LinkTheme } from './components/Links';
-import SampleAvatar from "./assets/png/sample-avatar.png";
+import Config from "./data/config.json";
 
 function App() {
   return (
     <div className="App">
       <Header
-        avatarOptions={{
-          title: "Haneen Mahdin",
-          description: "An aspiring full-stack developer and a UI/UX Designer",
-          avatarImageSource: SampleAvatar,
-        }}
+        avatarOptions={Config.avatarOptions}
 
-        stylingOptions={{
-          shadowType: AvatarShadowType.USE_DEFAULT_SHADOW,
-          isShadowAnimated: true
-        }}
+        stylingOptions={Config.stylingOptions}
       />
 
       <Links
-        links={[
-          {
-            linkUrl: "https://haneenmahd.github.io",
-            placeholder: "Portfolio ✨",
-          },
-          {
-            linkUrl: "https://github.com/haneenmahd",
-            placeholder: "GitHub",
-            colorSet: LinkTheme.GITHUB_THEME,
-          },
-          {
-            linkUrl: "https://dribbble.com/haneenmahdin",
-            placeholder: "Dribble",
-            colorSet: LinkTheme.DRIBBLE_THEME,
-          },
-          {
-            linkUrl: "https://instagram.com/haneenmahdin",
-            placeholder: "Instagram",
-            colorSet: LinkTheme.INSTAGRAM_THEME,
-          },
-          {
-            linkUrl: "mailto:admin@example.com",
-            placeholder: "Mail me ✉️",
-            colorSet: LinkTheme.PORTFOLIO_THEME,
-          },
-        ]}
+        links={Config.links}
       />
     </div>
   );
